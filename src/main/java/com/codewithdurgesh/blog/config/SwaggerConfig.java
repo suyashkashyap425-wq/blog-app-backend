93016ad9ddfb645df8a3a77d7cdee7f2001eaa1f
Suyash Kashyap <suyashkashyap425@gmail.com>
2026-02-23 16:11:56 +0530

    Force HTTPS in Swagger properly

--- src/main/java/com/codewithdurgesh/blog/config/SwaggerConfig.java
+++ src/main/java/com/codewithdurgesh/blog/config/SwaggerConfig.java
@@ -2,10 +2,13 @@ package com.codewithdurgesh.blog.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.servers.Server;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -13,9 +16,12 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
 
+        String serverUrl = "https://blog-app-backend-production-f8e2.up.railway.app";
+
         final String securitySchemeName = "bearerAuth";
 
         return new OpenAPI()
+                .servers(List.of(new Server().url(serverUrl)))
                 .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
                 .components(new Components()
                         .addSecuritySchemes(securitySchemeName,
